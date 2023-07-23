19d79d96880a5af4a086d83d8ae4665dc0b5c33c
Clay Risser <clayrisser@gmail.com>
2023-07-23 11:04:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,9 @@ sudo apt-get install -y --no-install-recommends \
     wayvnc \
     xwayland
 
+curl -L https://gitlab.com/bitspur/community/swayvnc/-/raw/main/swayvnc.sh | sudo tee /usr/local/bin/swayvnc
+sudo chmod +x /usr/local/bin/swayvnc
+
 openssl req -x509 -newkey rsa:4096 -sha256 -days 999999 -nodes \
     -keyout key.pem -out cert.pem -subj /CN=localhost \
     -addext subjectAltName=DNS:localhost,DNS:localhost,IP:127.0.0.1
