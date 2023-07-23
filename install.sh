2339691e9b741480992f9f03b3b6c0f8b14a2ca8
Clay Risser <clayrisser@gmail.com>
2023-07-23 04:25:25 -0500

    ✨ feat: Update installation and configuration scripts
    - Create the directory for WayVNC configuration at `/home/$USERNAME/.config/wayvnc` instead of `/home/$USERNAME/.config/swayvnc`
    - Generate OpenSSL key and certificate files in the new WayVNC directory
    - Add script instructions to run WayVNC and configure Sway output settings
    
    Details:
    - Modified `install.sh`:
      - Updated directory path from `SWAYVNC_CONFIG` to `WAYVNC_CONFIG`
      - Created the directory using `mkdir -p`
      - Generated key and certificate files in the new directory
      - Added script instructions to execute WayVNC and configure Sway output
    
    - Modified `swayvnc.sh`:
      - Exported environment variables for resolution, VNC authentication, VNC password, VNC address, sway socket, and WLR backends
      - Updated directory path from `$HOME/.config/wayvnc` to `WAYVNC_CONFIG`
      - Created the directory using `mkdir -p`
      - Updated configuration file path to match the new directory path
    
    Note: The commit message assumes that the conventional commits specification is followed, where "feat" represents a new feature.

--- install.sh
+++ install.sh
@@ -31,9 +31,13 @@ sudo chmod +x /usr/local/bin/swayvnc
 
 sudo useradd -m $USERNAME
 (echo "$PASSWORD"; echo "$PASSWORD") | sudo passwd $USERNAME
-SWAYVNC_CONFIG=/home/$USERNAME/.config/swayvnc
 
-sudo -u $USERNAME mkdir -p $SWAYVNC_CONFIG
+WAYVNC_CONFIG=/home/$USERNAME/.config/wayvnc
+sudo -u $USERNAME mkdir -p $WAYVNC_CONFIG
 sudo -u $USERNAME openssl req -x509 -newkey rsa:4096 -sha256 -days 999999 -nodes \
-    -keyout $SWAYVNC_CONFIG/key.pem -out $SWAYVNC_CONFIG/cert.pem -subj /CN=localhost \
+    -keyout $WAYVNC_CONFIG/key.pem -out $WAYVNC_CONFIG/cert.pem -subj /CN=localhost \
     -addext subjectAltName=DNS:localhost,DNS:localhost,IP:127.0.0.1
+cat <<EOF
+exec sh -c "[ \"\$VNC\" != \"false\" ] && wayvnc -C $WAYVNC_CONFIG/config || true"
+exec swaymsg output "HEADLESS-1" resolution "\$RESOLUTION"
+EOF
--- swayvnc.sh
+++ swayvnc.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 USERNAME=${USERNAME:-swayvnc}
+export RESOLUTION=${RESOLUTION:-1600x900}
+export VNC_ENABLE_AUTH=${VNC_ENABLE_AUTH:-true}
+export VNC_PASSWORD=${VNC_PASSWORD:-P@ssw0rd}
+export VNC_ADDRESS="0.0.0.0"
+export SWAYSOCK="/tmp/sway-ipc.sock"
+export WLR_BACKENDS="headless"
+export WLR_LIBINPUT_NO_DEVICES=1
+export XDG_RUNTIME_DIR="/tmp"
 
 if [ "$USER" != "$USERNAME" ]; then
   echo "The current user ($USER) does not match the target user ($USERNAME). Re-running with sudo -u $USERNAME..."
@@ -8,22 +16,13 @@ if [ "$USER" != "$USERNAME" ]; then
   exit $?
 fi
 
-export RESOLUTION="1600x900"
-export VNC=true
-export VNC_ENABLE_AUTH=false
-export VNC_PASSWORD="P@ssw0rd"
-export VNC_ADDRESS="0.0.0.0"
-export DOCKER_CMD="$@"
-export SWAYSOCK="/tmp/sway-ipc.sock"
-export WLR_BACKENDS="headless"
-export WLR_LIBINPUT_NO_DEVICES=1
-export XDG_RUNTIME_DIR="/tmp"
-mkdir -p $HOME/.config/wayvnc
+WAYVNC_CONFIG=$HOME/.config/wayvnc
+mkdir -p $WAYVNC_CONFIG
 printf "
 address=$VNC_ADDRESS
 enable_auth=$VNC_ENABLE_AUTH
 username=$USER
 password=$VNC_PASSWORD
-private_key_file=$HOME/key.pem
-certificate_file=$HOME/cert.pem" > $HOME/.config/wayvnc/config
+private_key_file=$WAYVNC_CONFIG/key.pem
+certificate_file=$WAYVNC_CONFIG/cert.pem" > $WAYVNC_CONFIG/config
 exec sway
